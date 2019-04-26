e5702579755df317d2379b3a35170e0a57d00210
Nguyễn Duy Lâm <45654466+NguyenDuyLam@users.noreply.github.com>
2019-04-26 10:06:13 +0700

    Validate Login
    validate

---
+++ validate/js/index.js
@@ -0,0 +1,41 @@
+    $(document).ready(function(){
+        $(".text-success").hide().html();
+        $(".text-danger").hide().html();
+        $(".text-info").hide().html();
+        $("#submit").click(function(){
+
+            var username=$("#name").val();
+            var password=$("#pass").val();
+            if(username == 'admin' && password == 123){
+                $("#form").html('<h4 style="text-align:center">User Login Successfully</h4><a href="">Back</a>').css('color','green');
+            }else if(username == '' || password == ''){
+                $(".text-danger").html("Both Field Required.").show().fadeOut(4000);
+            }else{
+                $(".text-info").html("User Does Not Match.").show().fadeOut(4000);
+            }
+        });
+    });
+
+
+
+
+
+    ----------------------------------------
+
+
+
+    $(document).ready(function() {
+        function validate() {
+            var uname = $('#uname'),
+                    passwd = $('#passwd');
+                    repasswd = $('#passwd')
+            
+            if(uname === "" && passwd === "" && repasswd ==="") {
+                alert('Please enter the Username and Password !');
+                return false;
+            } else {
+                alert('Correct !');
+                return false;
+            }
+        }
+    });
